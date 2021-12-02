e6d78f50a001a01272ccc1c66a7a1f1774d02069
YuriKitt <92264634+YuriKitt@users.noreply.github.com>
2021-12-02 17:28:57 +0200

    all tasks of the second lesson

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(obj) {
+  let s = 0;
+  for (key in obj) {
+    if (typeof obj[key] == "number" && isFinite(obj[key])) {
+      s = s + obj[key];
+    }
+  }
+  return s;
 }
