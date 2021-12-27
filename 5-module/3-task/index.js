395d6afc26d0888655b2dd44bba1cc87a930865b
YuriKitt <92264634+YuriKitt@users.noreply.github.com>
2021-12-27 15:03:40 +0200

    all tasks of the fifth module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,25 @@
 function initCarousel() {
-  // ваш код...
+  const arrowRight = document.querySelector(".carousel__arrow_right");
+  const arrowLeft = document.querySelector(".carousel__arrow_left");
+  const carousel = document.querySelector(".carousel__inner");
+  const slidesQtty = carousel.querySelectorAll(".carousel__slide").length;
+  const wid = carousel.offsetWidth;
+  let position = 0;
+  arrowLeft.style.display = "none";
+  arrowLeft.addEventListener("click", () => {
+    position = position + wid;
+    carousel.style.transform = `translateX(${position}px)`;
+    arrowRight.style.display = "";
+    position == 0
+      ? (arrowLeft.style.display = "none")
+      : (arrowLeft.style.display = "");
+  });
+  arrowRight.addEventListener("click", () => {
+    position = position - wid;
+    carousel.style.transform = `translateX(${position}px)`;
+    arrowLeft.style.display = "";
+    position == wid * (1 - slidesQtty)
+      ? (arrowRight.style.display = "none")
+      : (arrowRight.style.display = "");
+  });
 }
