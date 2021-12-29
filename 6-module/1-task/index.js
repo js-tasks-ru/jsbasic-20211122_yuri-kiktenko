b90d4a93cdfdf7a9a21a82b0140546a67515ff4f
YuriKitt <92264634+YuriKitt@users.noreply.github.com>
2021-12-30 01:30:37 +0200

    the first task of the sixth module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,38 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.elem = render(rows);
   }
 }
+function render(rows) {
+  const table = document.createElement("table");
+  table.innerHTML = `
+  <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        ${rows.map((row) => {
+            return `<tr>
+  <td>${row.name}</td>
+  <td>${row.age}</td>
+  <td>${row.salary}</td>
+  <td>${row.city}</td>
+  <td><button>X</button></td>
+</tr>`;}).join("")}
+    </tbody>`;
+  const buttons = table.querySelectorAll("button");
+  for (const button of buttons) {
+    button.addEventListener('click', (event) =>
+      event.target.closest("tr").remove());
+  }
+  return table;
+}
+// let q = this.render();
+// get elem() {
+//   return this.render();
+// }
