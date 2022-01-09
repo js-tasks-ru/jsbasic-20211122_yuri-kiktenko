64e2f8ccdd2656f7626ffda7dfd43d35137c7737
YuriKitt <92264634+YuriKitt@users.noreply.github.com>
2022-01-09 23:14:51 +0200

    the third task of the sixth module and some refactoring of the first two

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,10 +1,10 @@
 export default class UserTable {
   constructor(rows) {
-    this.elem = this.render(rows);
-  }
-  render(rows) {
-    const table = document.createElement("table");
-    table.innerHTML = `
+    this.elem = createTable(rows);
+
+    function createTable(rows) {
+      const table = document.createElement("table");
+      table.innerHTML = `
     <thead>
         <tr>
             <th>Имя</th>
@@ -15,22 +15,26 @@ export default class UserTable {
         </tr>
     </thead>
     <tbody>
-        ${rows.map((row) => {return `
+        ${rows
+          .map((row) => {
+            return `
         <tr>
             <td>${row.name}</td>
             <td>${row.age}</td>
             <td>${row.salary}</td>
             <td>${row.city}</td>
             <td><button>X</button></td>
-        </tr>`;}).join("")}
+        </tr>`;
+          })
+          .join("")}
     </tbody>`;
-    table.addEventListener("click", this.onClick);
-    return table;
-  }
-
-  onClick(event) {
-    if (event.target.tagName === "BUTTON") {
-      event.target.closest("tr").remove();
+      table.addEventListener("click", onClick);
+      function onClick(event) {
+        if (event.target.tagName === "BUTTON") {
+          event.target.closest("tr").remove();
+        }
+      }
+      return table;
     }
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,74 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.elem = this.render();
+    this.initCarousel();
+  }
+  render() {
+    return createElement(`
+  <div class="carousel">
+    <div class="carousel__arrow carousel__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </div>
+    <div class="carousel__arrow carousel__arrow_left">
+      <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+    </div>
+
+    <div class="carousel__inner">` +
+        this.slides.map((slide) => `
+      <div class="carousel__slide" data-id=${slide.id}>
+        <img src="/assets/images/carousel/${
+          slide.image
+        }" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+          <div class="carousel__title">${slide.name}</div>
+            <button type="button" class="carousel__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+        </div>
+      </div>`).join("") + `
+    </div>
+  </div>`
+    );
+  }
+
+  initCarousel() {
+    const arrowRight = this.elem.querySelector(".carousel__arrow_right");
+    const arrowLeft = this.elem.querySelector(".carousel__arrow_left");
+    const carousel = this.elem.querySelector(".carousel__inner");
+    const slidesQtty = carousel.querySelectorAll(".carousel__slide").length;
+    let position = 0;
+    arrowLeft.style.display = "none";
+    arrowLeft.addEventListener("click", () => {
+      position = position + carousel.offsetWidth;
+      carousel.style.transform = `translateX(${position}px)`;
+      arrowRight.style.display = "";
+      position == 0
+        ? (arrowLeft.style.display = "none")
+        : (arrowLeft.style.display = "");
+    });
+    arrowRight.addEventListener("click", () => {
+      position = position - carousel.offsetWidth;
+      carousel.style.transform = `translateX(${position}px)`;
+      arrowLeft.style.display = "";
+      position == carousel.offsetWidth * (1 - slidesQtty)
+        ? (arrowRight.style.display = "none")
+        : (arrowRight.style.display = "");
+    });
+    carousel.addEventListener("click", this.onClick);
+  }
+
+  onClick(event) {
+    if (event.target.closest("button.carousel__button")) {
+      const id = event.target.closest("div.carousel__slide").dataset.id;
+      const userEvent = new CustomEvent("product-add", {
+        detail: id,
+        bubbles: true,
+      });
+      this.dispatchEvent(userEvent);
+    }
   }
 }
